455ada2994ebc29567b9582b142d221d696b982a
Faner <fanerstder@gmail.com>
2026-08-07 13:46:37 -0500

    Actualización de icono tacos

--- POC/app/src/main/kotlin/com/tacoos/poc/data/remote/TacoApi.kt
+++ POC/app/src/main/kotlin/com/tacoos/poc/data/remote/TacoApi.kt
@@ -67,7 +67,7 @@ interface TacoApi {
     
     companion object {
         //const val BASE_URL = "http://192.168.1.144:8080/"
-        //const val BASE_URL = "http://192.168.1.7:8080/"
-        const val BASE_URL = "http://10.0.2.2:8080/"
+        const val BASE_URL = "http://192.168.1.6:8080/"
+        //const val BASE_URL = "http://10.0.2.2:8080/"
     }
 }
